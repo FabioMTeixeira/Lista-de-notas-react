e788e336b44b68530064b17ae278bff60d66848a
Fabio <fabiomt.98@hotmail.com>
2023-03-08 11:20:58 -0300

    did new components and finished the first page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,23 +1,47 @@
-import { Container } from './styles';
+import { Container, Links, Content } from './styles';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Button title="Voltar" />
+          <h1>Introdução a React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+            quae dolore nostrum similique non eaque officia eligendi quod, ipsa,
+            tempore exercitationem numquam in! Eligendi dignissimos quam autem,
+            ipsa doloremque porro.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node.js" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
